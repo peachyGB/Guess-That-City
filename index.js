520ec0ea476d3d685e0fb42cf12c57eb1f7ff677
Georgia Burnside <burnsidegeorgia@gmail.com>
2022-04-20 17:48:06 -0400

    yup

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
+  let moscowImgSrc = fetch(
+    "https://api.teleport.org/api/urban_areas/slug:moscow/images/"
+  )
+    .then((response) => response.json())
+    .then((moscowImage) => handleImg(moscowImage));
+  // let mexicoCityImgSrc = fetch(​​'https://api.teleport.org/api/urban_areas/slug:mexico-city/images/')
+  //   .then((response) => response.json())
+  //   .then((mexicoCityImage) => handleImg(mexicoCityImage));
+  // let tokyoImgSrc = fetch(​​'https://api.teleport.org/api/urban_areas/slug:tokyo/images/')
+  //   .then((response) => response.json())
+  //   .then((tokyoImage) => handleImg(tokyoImage));
+  //     let barcelonaImgSrc = fetch(​​'https://api.teleport.org/api/urban_areas/slug:barcelona/images/')
+  //   .then((response) => response.json())
+  //   .then((barcelonaImage) => handleImg(barcelonaImage));
+  //     let sanFranciscoImgSrc = fetch(​​'https://api.teleport.org/api/urban_areas/slug:san-francisco-bay-area/images/')
+  //   .then((response) => response.json())
+  //   .then((sanFranciscoImage) => handleImg(sanFranciscoImage));
 
-fetch("https://api.teleport.org/api/cities/")
-  .then((response) => response.json())
-  .then((data) => handleData(data));
+  function handleImg(data) {
+    let photoBin = document.getElementById("cityPhoto");
+    photoBin.src = data.photos[0].image.mobile;
+    console.log(photoBin);
+    /* brain tasks
+insert photos from api*/
+  }
+});
+//_links -> ua:item -> [index] -> _links -> ua:images -> photos -> image -> moble
 
-
-  
-}
+//add functionality to submit forms
+////confirm tru or false city name
+//pull images from api
+//pull city info from api
+//add hover event to display city info
+//
